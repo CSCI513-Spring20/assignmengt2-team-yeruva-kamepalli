019be4e00353599c31f1f5b88fb39f71fe84a422
varun anish reddy <varun anish reddy@LAPTOP-RJ86IJM2>
2020-02-17 19:18:24 -0600

    Added pirate islands

--- Assignment2/src/OceanMap.java
+++ Assignment2/src/OceanMap.java
@@ -1,5 +1,16 @@
 import java.awt.Point;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Random;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 public class OceanMap  {
 
@@ -31,4 +42,37 @@ return new Point(0,0);
 }
 
 }
+static void addpirateIslands(AnchorPane root) {
+	final int scale = 50;
+	 final int dimension=10;
+    FileInputStream fileInputStream1 = null;
+    try {
+        fileInputStream1 = new FileInputStream("src/pirateIsland.png");
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    }
+    
+    Image pirateIsland = new Image(fileInputStream1,50,50,true,true);
+  
+            
+            //PIRATE ISLAND
+            ImageView pirateIslandView = new ImageView(pirateIsland);
+            pirateIslandView.setX(3*scale);
+            pirateIslandView.setY(7*scale);
+      
+           // pirateIslandView.setFitHeight(60.0);
+           // pirateIslandView.setFitWidth(60.0);
+            
+            //PIRATE ISLAND
+            ImageView pirateIslandView1 = new ImageView(pirateIsland);
+            
+            pirateIslandView.setX(7*scale);
+            pirateIslandView.setY(3*scale);
+            root.getChildren().add(pirateIslandView);
+            root.getChildren().add(pirateIslandView1);
+        
+        
+    
+}
+
 }
